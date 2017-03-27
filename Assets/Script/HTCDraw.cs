49c84441db250563138d31d3b5e5d06f008cad54
sika0819 <740015523@qq.com>
2017-03-27 16:06:16 +0800

    HTC画笔

---
+++ Assets/Script/HTCDraw.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class HTCDraw : MonoBehaviour
+{
+    public Material mat;
+    public Color color = Color.white;
+    public GameObject brush;
+    GameObject tempBrush;
+    LineRenderer brushData;
+    public List<Vector3> pointList;
+    bool startDraw=false;
+    public bool isReady = false;
+    public float freshRate = 25f;//刷新频率多少帧
+    float freshTimer = 0;
+    SteamVR_TrackedObject trackObj;
+    void Start()
+    {
+        pointList = new List<Vector3>();
+        mat.color = color;
+        trackObj = GetComponent<SteamVR_TrackedObject>();
+    }
+    void Update()
+    {
+        var device = SteamVR_Controller.Input((int)trackObj.index);
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {//更换成HTC手柄输入即可
+            startDraw = true;
+            pointList = new List<Vector3>();
+            tempBrush = Instantiate(brush) as GameObject;
+            tempBrush.GetComponent<Renderer>().material = mat;
+            mat.color = color;
+            brushData = tempBrush.GetComponent<LineRenderer>();
+        }
+        if (startDraw)
+        {
+            Vector3 Point = transform.position;
+            freshTimer += Time.deltaTime;
+            if (freshTimer > (1 / freshRate))
+            {
+                freshTimer = 0;
+                pointList.Add(Point);
+                if (pointList.Count > 1)
+                {
+                    brushData.numPositions = pointList.Count;
+                    brushData.SetPositions(pointList.ToArray());
+                }
+            }
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            startDraw = false;
+            pointList.Clear();
+        }
+    }
+}
---
+++ Assets/Script/Test.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Test : MonoBehaviour
+{
+    public Material mat;
+    public Color color = Color.white;
+    public GameObject brush;
+    GameObject tempBrush;
+    LineRenderer brushData;
+    public List<Vector3> pointList;
+    bool startDraw = false;
+    public bool isReady = false;
+    public float freshRate = 25f;//刷新频率多少帧
+    float freshTimer = 0;
+    SteamVR_TrackedObject trackObj;
+    public GameObject ColorPicker;
+    public GameObject mouseUI;
+    void Start()
+    {
+        pointList = new List<Vector3>();
+        mat.color = color;
+        trackObj = GetComponent<SteamVR_TrackedObject>();
+        mouseUI = ColorPicker.transform.FindChild("Mouse").gameObject;
+        ColorPicker.SetActive(false);
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {//更换成HTC手柄输入即可
+            startDraw = true;
+            pointList = new List<Vector3>();
+            tempBrush = Instantiate(brush) as GameObject;
+            tempBrush.GetComponent<Renderer>().material = mat;
+            mat.color = color;
+            brushData = tempBrush.GetComponent<LineRenderer>();
+        }
+        if (startDraw)
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 Point = Camera.main.ScreenToWorldPoint(mousePos);
+            freshTimer += Time.deltaTime;
+            if (freshTimer > (1 / freshRate))
+            {
+                freshTimer = 0;
+                pointList.Add(Point);
+                if (pointList.Count > 1)
+                {
+                    brushData.numPositions = pointList.Count;
+                    brushData.SetPositions(pointList.ToArray());
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            startDraw = false;
+            pointList.Clear();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            ColorPicker.SetActive(true);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 Point = Camera.main.ScreenToWorldPoint(mousePos);
+            mouseUI.transform.localPosition = Point;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            ColorPicker.SetActive(false);
+        }
+    }
+}
